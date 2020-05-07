e1e328d5b0153bbaac5f1560c031dbb039112bd4
nandan_abhishek <54950560+nandanabhishek@users.noreply.github.com>
2020-05-07 17:32:31 +0530

    Add files via upload

---
+++ abhi.py
@@ -0,0 +1,7 @@
+p=float(input('enter principal: '))
+r=float(input('enter rate of interest: '))
+t=float(input('enter time: '))
+      
+si=(p * r * t)/100
+      
+print('The Simple Interest is', si) 
