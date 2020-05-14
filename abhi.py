fb73b601574b79e56246c895984195c5497c7a76
Abhishek Nandan <54950560+nandanabhishek@users.noreply.github.com>
2020-05-14 23:27:52 +0530

    Add files via upload

---
+++ abhi.py
@@ -0,0 +1,13 @@
+# Sum of natural numbers up to num
+
+num = int(input("enter a positive number: "))
+a=num
+if num < 0:
+   print("Enter a positive number")
+else:
+   sum = 0
+   # use while loop to iterate until zero
+   while(num > 0):
+       sum += num
+       num -= 1
+   print("The sum of positive integers upto "+str(a)+" is "+ str(sum))
