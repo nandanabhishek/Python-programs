d3c469053e029380ba54eeb0c03af2152e07096a
Abhishek Nandan <54950560+nandanabhishek@users.noreply.github.com>
2020-05-14 22:59:33 +0530

    Add files via upload

---
+++ abhi.py
@@ -0,0 +1,9 @@
+def swap(a,b):
+    print("Before swapping: "+"a="+str(a)+" ,b="+str(b))
+    k=a
+    a=b
+    b=k
+    print("After swapping: "+"a="+str(a)+" ,b="+str(b))
+c=input("enter a no:")
+d=input("enter another no:")
+swap(c,d)
