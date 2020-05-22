61696b17e1ed4330aa7df2ba8d6f341cb365eeb1
Abhishek Nandan <54950560+nandanabhishek@users.noreply.github.com>
2020-05-22 22:56:09 +0530

    Rename abhi.py to string.upper()_function.py

--- abhi.py
+++
@@ -1,3 +0,0 @@
-a=input('enter any string: ')
-print('you have entered: ',a)
-print('string converted to upper case letter is :',a.upper())
---
+++ string.upper()_function.py
@@ -0,0 +1,3 @@
+a=input('enter any string: ')
+print('you have entered: ',a)
+print('string converted to upper case letter is :',a.upper())
