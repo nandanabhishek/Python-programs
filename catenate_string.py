0e174a2380ffb77363ae46f060e46338fdf371b3
Abhishek Nandan <54950560+nandanabhishek@users.noreply.github.com>
2020-05-22 22:32:07 +0530

    Add files via upload

---
+++ catenate_string.py
@@ -0,0 +1,10 @@
+# taking two string from user
+a=input('Enter 1st string:')
+b=input('Enter 2nd string:')
+
+# printing both strings together
+print('str1+str2=',a+b)
+
+# printing string 1 three times
+print('str1*3=',a*3)
+
